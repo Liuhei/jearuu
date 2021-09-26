b9254cb689f6e2357cd1c79c0cfbf6a8b20d0087
Liuhei <liuhei.yamada@gmail.com>
2021-09-26 12:22:27 +0900

    after finishing css/js for tablet/mobile

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ window.onload = function(){
 		$( this ).css( "display", "none" );
 	});
 
-	//showing boxes when it's inside the viewport
+	//show boxes when it's inside the viewport
 	function isInsideViewport( el ){
 		const rect = el.getBoundingClientRect();
 		return ( rect.top - window.innerHeight + 100 < 0 );
@@ -37,6 +37,7 @@ window.onload = function(){
 	const info_box = document.getElementById( 'info-wrap' );
 	const left_box = document.querySelector( '.info-left' );
 	const right_box = document.querySelector( '.info-right' );
+	const footer_logo = document.querySelector( '.footer-logo' );
 	console.log(image_box);
 	document.addEventListener( 'scroll', function(){
 		//check if the image is inside the viewport
@@ -48,9 +49,11 @@ window.onload = function(){
 			left_box.classList.add( "left-show" );
 			right_box.classList.add( "right-show" );
 		}
+		//check if the footer title is inside the viewport
+		if ( isInsideViewport( footer_logo ) ){footer_logo.classList.add( "text-show" );}
 	});
 
-	//setting images for each box
+	//set image for each box
 	const img_array = [
 		"images/pexels-helena-lopes-1862000.jpg",
 		"images/pexels-kevin-villaruz-1660603.jpg",
@@ -65,24 +68,28 @@ window.onload = function(){
 		"images/pexels-umar-mukhtar-1538177.jpg",
 		"images/pexels-aleksandar-pasaric-3372408.jpg"
 	];
-	//images for three columns
-	const c1 = document.getElementById( 'gallary-column-1' ).getElementsByTagName( 'img' );
-	const c2 = document.getElementById( 'gallary-column-2' ).getElementsByTagName( 'img' );
-	const c3 = document.getElementById( 'gallary-column-3' ).getElementsByTagName( 'img' );
+	//set images for three columns
+	const c1 = document.getElementById( 'gallary-column-1' );
+	const c2 = document.getElementById( 'gallary-column-2' );
+	const c3 = document.getElementById( 'gallary-column-3' );
 	for (var i = 0; i < img_array.length/3; i++){
-		c1[i].src = img_array[i*3]
+		c1.getElementsByTagName( 'img' )[i].src = img_array[i*3]
 	}
 	for (var i = 0; i < img_array.length/3; i++){
-		c2[i].src = img_array[i*3+1]
+		c2.getElementsByTagName( 'img' )[i].src = img_array[i*3+1]
 	}
 	for (var i = 0; i < img_array.length/3; i++){
-		c3[i].src = img_array[i*3+2]
+		c3.getElementsByTagName( 'img' )[i].src = img_array[i*3+2]
 	}
-	//test
+	//change layout when the screen size changes
 	window.onresize = function(){
-		console.log("resize");
+		const screen_width = window.screen.width;
+		if (screen_width < 768 ){
+			c3.style.display = "none"
+		} else {
+			c3.style.display = "block"
+		}
 	}
-	//test end
 }
 
 
